4ab297ebc760c75c2f95f2d15f2573e753eaad4a
Max Skorokhodov <skormaksim@mail.ru>
2023-05-16 12:12:23 +0300

    feat: добавить Hheader, Footer в App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from '../parts/Header/Header';
+import Footer from '../parts/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page">
+        <Header
+          // loggedIn={loggedIn}
+          // userData={email}
+          // onSignOut={handleSignOut}
+        />
+
+        <div className="wrapper"></div>
+
+        <Footer />
+      </div>
     </div>
   );
 }
