fb751b3120e71142447b3aa5c692d553b7fe1afe
Max Skorokhodov <skormaksim@mail.ru>
2023-05-18 14:19:55 +0300

    feat: сверстать компонент Navigation

--- src/components/parts/Navigation/Navigation.js
+++ src/components/parts/Navigation/Navigation.js
@@ -5,20 +5,26 @@ import { ROOT_URL } from '../../../utils/settings';
 function Navigation() {
   return (
     <nav className="menu">
-      <div className="menu-films">
-        <Link to={`${ROOT_URL}films`} className="menu-films__link menu-films__link_type_burger">
-          Главная
-        </Link>
-        <Link to={`${ROOT_URL}films`} className="menu-films__link">
-          Фильмы
-        </Link>
-        <Link to={`${ROOT_URL}films`} className="menu-films__link">
-          Сохраненные фильмы
-        </Link>
-      </div>
+      <ul className="menu-films">
+        {/* <li>
+          <Link to={`${ROOT_URL}films`} className="menu-films__link menu-films__link_type_burger">
+            Главная
+          </Link>
+        </li> */}
+        <li>
+          <Link to={`${ROOT_URL}films`} className="menu-films__link menu-films__link_active">
+            Фильмы
+          </Link>
+        </li>
+        <li>
+          <Link to={`${ROOT_URL}films`} className="menu-films__link">
+            Сохраненные фильмы
+          </Link>
+        </li>
+      </ul>
 
-      <div className="menu-auth">
-        <div className="menu-login">
+      <nav className="menu-auth">
+        {/* <div className="menu-login">
           <Link to={`${ROOT_URL}films`} className="menu-login__link">
             Регистрация
           </Link>
@@ -27,8 +33,8 @@ function Navigation() {
             // onClick={handelBtnBurgerClick}
             type="button"
             aria-label="переход на страницу авторизации"
-          ></button>
-        </div>
+          >Войти</button>
+        </div> */}
         <div className="menu-profile">
           <Link to={`${ROOT_URL}films`} className="menu-profile__link">
             Аккаунт
@@ -40,7 +46,7 @@ function Navigation() {
             aria-label="кнопка выхода из профиля"
           ></button>
         </div>
-      </div>
+      </nav>
     </nav>
   );
 }
