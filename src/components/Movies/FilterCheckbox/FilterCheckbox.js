9183598bfa173fc8a749191733759f7120d61d52
Max Skorokhodov <skormaksim@mail.ru>
2023-06-09 12:09:10 +0300

    feat: сделать адаптив SearchPanel

--- src/components/Movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -4,13 +4,9 @@ function FilterCheckbox() {
   return (
     <div className="filter">
       <label className="checkbox">
-        <input
-          className="checkbox__input"
-          type="checkbox"
-          name="filter"
-        />
+        <input className="checkbox__input" type="checkbox" name="filter" />
         <div className="checkbox__content">
-          <div className="checkbox__switch"/>
+          <div className="checkbox__switch" />
         </div>
         <div className="checkbox__text">Короткометражки</div>
       </label>
--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -3,16 +3,13 @@ import './SearchForm.css';
 function FilterCheckbox() {
   return (
     <form className="search-form">
-      <div className="search-form__icon" />
-      <input
-        className="search-form__input"
-        placeholder="Фильм"
-        type="text"
-        name=""
-      />
-      <button
-        className="search-form__submit"
-        type="submit"><span className="search-form__submit-inner"/></button>
+      <label className="search-form__field">
+        <div className="search-form__icon" />
+        <input className="search-form__input" placeholder="Фильм" type="text" name="" />
+      </label>
+      <button className="search-form__submit" type="submit">
+        <span className="search-form__submit-inner" />
+      </button>
     </form>
   );
 }
