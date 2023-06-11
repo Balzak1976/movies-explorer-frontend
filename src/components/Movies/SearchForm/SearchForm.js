31ab37b1e664974ad04cadadcb2b97624e693985
Max Skorokhodov <skormaksim@mail.ru>
2023-06-11 12:17:22 +0300

    fear: валидировать search-form

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,13 +1,35 @@
 import './SearchForm.css';
+import { useFormAndValidation } from '../../../hooks/useFormAndValidation';
 
-function FilterCheckbox() {
+function FilterCheckbox({ onSearchForm }) {
+  const { values, handleChange, errors, isValid } = useFormAndValidation();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    onSearchForm(values);
+  };
   return (
-    <form className="search-form">
+    <form className="search-form" name={'search-form'} onSubmit={onSubmit} noValidate>
       <label className="search-form__field">
         <span className="search-form__icon" />
-        <input className="search-form__input" placeholder="Фильм" type="text" name="movies" />
+        <input
+          value={values.movie ?? ''}
+          onChange={handleChange}
+          className="search-form__input"
+          placeholder="Фильм"
+          type="text"
+          name="movie"
+          required
+        />
+        <span className={`search-form__input-error ${errors && 'search-form__input-error_active'}`}>
+          {errors.movie && "Нужно ввести ключевое слово"}
+        </span>
       </label>
-      <button className="search-form__submit" type="submit">
+      <button
+        className={`search-form__submit ${!isValid && 'search-form__submit_inactive'}`}
+        type="submit"
+        disabled={!isValid}>
         <span className="search-form__submit-inner" />
       </button>
     </form>
