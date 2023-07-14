5cf1ace6e3e9dc52edbac9dd9e071b4b60d61849
Max Skorokhodov <skormaksim@mail.ru>
2023-07-14 22:50:02 +0300

    feat: сделать компонент LogoSvg

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,11 +1,11 @@
 import './Logo.css';
-import logo from '../../../images/logo.svg';
+import { ReactComponent as LogoSvg } from '../../../images/logo.svg';
 import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <Link to="/" >
-      <img className="logo" src={logo} alt="логотип" />
+    <Link to="/">
+      <LogoSvg className="logo" />
     </Link>
   );
 }
